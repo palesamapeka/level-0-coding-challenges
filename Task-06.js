d0b3367e907c7a1c85bb36a06924793617c7aa09
Palesa Mapeka <palesamapeka97@gmail.com>
2021-08-16 16:11:07 +0200

    fixed the bug. Now returns the maximum negative number

--- Task-06.js
+++ Task-06.js
@@ -1,9 +1,11 @@
-var max = 0;
+//var max = 0;
 
 function maximum()
 {
     for(var x = 0; x <arguments.length; x++)
     {
+        var valuesArgs = Array.prototype.slice.call(arguments);
+        var max = valuesArgs[0];
         if(arguments[x] >= max)
         {
             max = arguments[x];
@@ -12,4 +14,4 @@ function maximum()
     console.log(max);
 }
 
-maximum(1,22,3,2);
+maximum(-10, -9, -8);
