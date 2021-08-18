e64b99010a85b2c0c540358a8a4ba55aa569e4ff
Palesa Mapeka <palesamapeka97@gmail.com>
2021-08-18 16:30:18 +0200

    I realised my mistake. I think I fixed it now.

--- Task-08.js
+++ Task-08.js
@@ -1,44 +1,43 @@
 function convertToTime(inputNumber)
 {
     if(parseInt(inputNumber) <= 0)
-    {
+      {
         console.log("Unable to calculate time")
-    }
-    else {
+      }
+    else 
+    {
         var hours = Math.floor((parseInt(inputNumber) / 60));
         var minutes = Math.round((parseInt(inputNumber) % 60));
-
-        if(hours === 1 && minutes === 1)
-           {
-                console.log(hours + " hour and " +  minutes + " minute");
-           }
-        else if(hours >= 2 && minutes >= 2)
-           {
-                console.log(hours + " hours and " + minutes + " mniutes");
-           }
-
-        else if(hours <= 1 && minutes > 1)
-            {
-                console.log(hours + " hour and " + minutes + " minutes");
+        
+            if (hours > 1 && minutes > 1){
+                    console.log(hours + " hours and " + minutes + " minutes." );
             }
 
-        else if(hours >= 2  && minutes <= 1)
-            {
-                 console.log(hours + " hours and " + minutes + " minute");
+            else if (hours === 1 && minutes === 1){
+                    console.log(hours + " hour and " + minutes + " minute.");
             }
-        else if(hours === 0 && minutes > 1)
-           {
-                 console.log(minutes + " minutes") || console.log(minutes + " minute");
-           }
-        else if(hours > 1 && minutes === 0)
-        {
-                 console.log(hours + " hours") || console.log(hours + " hour");
-        }
-        else if(hours === 1 && minutes === 0)
-        {
-                 console.log(hours + " hour");
-        }
-    
-        }
+
+            else if (hours > 1 && minutes === 1){
+                    console.log(hours + " hours and " + minutes + " minute.");
+            }
+
+            else if (hours === 1 && minutes > 1){
+                    console.log(hours + " hour and " + minutes + " minutes.")
+            }
+
+            else if (hours === 1 && (minutes < 1 || minutes === 0)){
+                    console.log(hours + " hour.")
+            }
+
+            else if ((hours < 1 || hours === 0) && minutes === 1){
+                    console.log(minutes + " minute.")
+            }
+            else if (hours === 0 && minutes > 1){
+                    console.log(minutes + " minutes.");
+            }
+            else if (hours > 1 && minutes === 0){
+                    console.log(hours + " hours.");
+            }
     }
-convertToTime(55);
+}
+convertToTime(26);
